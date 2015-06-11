28d4ec202f1d59b70cd341ff459e592c11b399aa
Sven Kubiak <sk@svenkubiak.de>
2015-06-11 11:21:41 +0200

    fixed unit tests

--- mangooio-integration-test/src/main/java/controllers/I18nController.java
+++ mangooio-integration-test/src/main/java/controllers/I18nController.java
@@ -1,19 +1,9 @@
 package controllers;
 
-import com.google.inject.Inject;
-
-import mangoo.io.i18n.Messages;
 import mangoo.io.routing.Response;
 
 public class I18nController {
-
-	@Inject
-	private Messages messages;
-	
     public Response translation() {
-        messages.get("my.translation");
-        messages.get("my.othertranslation", "foo");
-
         return Response.withOk();
     }
 }
--- mangooio-integration-test/src/test/java/mangoo/controllers/I18nControllerTest.java
+++ mangooio-integration-test/src/test/java/mangoo/controllers/I18nControllerTest.java
@@ -2,10 +2,8 @@ package mangoo.controllers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import mangoo.io.testing.MangooRequest;
 import mangoo.io.testing.MangooResponse;
-import mangoo.io.testing.MangooUnit;
 
 import org.junit.Test;
 /**
@@ -13,21 +11,12 @@ import org.junit.Test;
  * @author svenkubiak
  *
  */
-public class I18nControllerTest extends MangooUnit {
-
+public class I18nControllerTest {
     @Test
     public void templateTest() {
         MangooResponse response = MangooRequest.get("/translation").header("Accept-Language", "de-DE").execute();
         
         assertNotNull(response.getContent());
         assertEquals("willkommen", response.getContent());
-    }
-    
-    @Test
-    public void title_of_bing_should_contain_search_query_name() {
-        goTo("http://www.bing.com");
-        fill("#sb_form_q").with("FluentLenium");
-        submit("#sb_form_go");
-        assertTrue(title().contains("FluentLenium"));
     }
 }
