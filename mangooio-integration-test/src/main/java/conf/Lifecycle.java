62e04aa85686d334707b46add6bf3f582d7eb542
Sven Kubiak <sk@svenkubiak.de>
2015-06-18 14:09:26 +0200

    remove white spaces

--- mangooio-integration-test/src/main/java/conf/Lifecycle.java
+++ mangooio-integration-test/src/main/java/conf/Lifecycle.java
@@ -7,13 +7,13 @@ import com.google.inject.Singleton;
 @Singleton
 public class Lifecycle implements MangooLifecycle {
 
-	@Override
-	public void applicationInitialized() {
-		//Do nothing for now		
-	}
-	
+    @Override
+    public void applicationInitialized() {
+        // Do nothing for now
+    }
+
     @Override
     public void applicationStarted() {
-        //Do nothing for now
+        // Do nothing for now
     }
 }
