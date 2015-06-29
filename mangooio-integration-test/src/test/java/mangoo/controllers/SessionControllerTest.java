bd1ead43255f3292d10b9955728b52246ffca095
Sven Kubiak <sk@svenkubiak.de>
2015-06-29 11:05:47 +0200

    fixed bug in unit tests

--- mangooio-integration-test/src/test/java/mangoo/controllers/SessionControllerTest.java
+++ mangooio-integration-test/src/test/java/mangoo/controllers/SessionControllerTest.java
@@ -5,14 +5,14 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
+import org.apache.http.cookie.Cookie;
+import org.junit.Test;
+
 import mangoo.io.configuration.Config;
-import mangoo.io.core.Application;
 import mangoo.io.enums.Key;
 import mangoo.io.test.MangooRequest;
 import mangoo.io.test.MangooResponse;
-
-import org.apache.http.cookie.Cookie;
-import org.junit.Test;
+import mangoo.io.test.MangooTest;
 
 /**
  * 
@@ -22,7 +22,7 @@ import org.junit.Test;
 public class SessionControllerTest {
     @Test
     public void sessionTest() {
-        Config config = Application.getInjector().getInstance(Config.class);
+        Config config = MangooTest.INSTANCE.getInjector().getInstance(Config.class);
         String cookieName = config.getString(Key.COOKIE_NAME);
         
         MangooResponse response = MangooRequest.get("/session").execute();
