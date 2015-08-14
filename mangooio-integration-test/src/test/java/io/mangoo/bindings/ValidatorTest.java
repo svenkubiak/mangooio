ca0c5fc7758b451429014d1c7665a80f0b04c938
Sven Kubiak <sk@svenkubiak.de>
2015-08-14 19:05:47 +0200

    completed validator unit tests

--- mangooio-integration-test/src/test/java/io/mangoo/bindings/ValidatorTest.java
+++ mangooio-integration-test/src/test/java/io/mangoo/bindings/ValidatorTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.regex.Pattern;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,12 +13,20 @@ import io.mangoo.routing.bindings.Validator;
 import io.mangoo.test.MangooTestInstance;
 
 public class ValidatorTest {
+    private Validator validator;
+    private static final String URL = "url";
+    private static final String REGEX = "regex";
+    private static final String RANGE = "range";
+    private static final String IPV6 = "ipv6";
+    private static final String IPV4 = "ipv4";
+    private static final String EMAIL = "email";
+    private static final String MATCH2 = "match2";
+    private static final String MATCH = "match";
     private static final String EXACT_MATCH = "exactMatch";
     private static final String MAX = "max";
     private static final String MIN = "min";
     private static final String REQUIRED = "required";
     private static final String CUSTOM_ERROR_MESSAGE = "Custom error message";
-    private Validator validator;
 
     @Before
     public void setup () {
@@ -180,27 +190,210 @@ public class ValidatorTest {
 
     @Test
     public void testMatchFailed() {
-        validator.add("match", "match");
-        validator.match("match", "foo");
+        validator.add(MATCH, MATCH);
+        validator.match(MATCH, "foo");
 
         assertTrue(validator.hasErrors());
-        assertTrue(validator.hasError("match"));
-        assertEquals("match must match foo", validator.getError("match"));
+        assertTrue(validator.hasError(MATCH));
+        assertEquals("match must match foo", validator.getError(MATCH));
 
-        validator.add("match", "match");
-        validator.exactMatch("match", "foo", CUSTOM_ERROR_MESSAGE);
+        validator.add(MATCH, MATCH);
+        validator.exactMatch(MATCH, "foo", CUSTOM_ERROR_MESSAGE);
 
         assertTrue(validator.hasErrors());
-        assertTrue(validator.hasError("match"));
-        assertEquals(CUSTOM_ERROR_MESSAGE, validator.getError("match"));
+        assertTrue(validator.hasError(MATCH));
+        assertEquals(CUSTOM_ERROR_MESSAGE, validator.getError(MATCH));
     }
 
     @Test
     public void testMatchSuccess() {
-        validator.add("match", "match");
-        validator.match("match", "mAtcH");
+        validator.add(MATCH, MATCH);
+        validator.add(MATCH2, "mAtcH");
+        validator.match(MATCH, MATCH2);
 
         assertFalse(validator.hasErrors());
-        assertFalse(validator.hasError("match"));
+        assertFalse(validator.hasError(MATCH));
+    }
+
+    @Test
+    public void testEmailFailed() {
+        validator.add(EMAIL, "foo @");
+        validator.email(EMAIL);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(EMAIL));
+        assertEquals("email must be a valid eMail address", validator.getError(EMAIL));
+
+        validator.add(EMAIL, "foo @");
+        validator.exactMatch(EMAIL, "foo", CUSTOM_ERROR_MESSAGE);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(EMAIL));
+        assertEquals(CUSTOM_ERROR_MESSAGE, validator.getError(EMAIL));
+    }
+
+    @Test
+    public void testEmailSuccess() {
+        validator.add(EMAIL, "foo@bar.com");
+        validator.email(EMAIL);
+
+        assertFalse(validator.hasErrors());
+        assertFalse(validator.hasError(EMAIL));
+    }
+
+    @Test
+    public void testIpv4Failed() {
+        validator.add(IPV4, "192.189.383.122");
+        validator.ipv4(IPV4);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(IPV4));
+        assertEquals("ipv4 must be a valid IPv4 address", validator.getError(IPV4));
+
+        validator.add(IPV4, "192.189.383.122");
+        validator.ipv4(IPV4, CUSTOM_ERROR_MESSAGE);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(IPV4));
+        assertEquals(CUSTOM_ERROR_MESSAGE, validator.getError(IPV4));
+    }
+
+    @Test
+    public void testIpvSuccess() {
+        validator.add(IPV4, "192.168.2.1");
+        validator.ipv4(IPV4);
+
+        assertFalse(validator.hasErrors());
+        assertFalse(validator.hasError(IPV4));
+    }
+
+    @Test
+    public void testIpv6Failed() {
+        validator.add(IPV6, "1f::::0");
+        validator.ipv6(IPV6);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(IPV6));
+        assertEquals("ipv6 must be a valid IPv6 address", validator.getError(IPV6));
+
+        validator.add(IPV6, "1f::::0");
+        validator.ipv6(IPV6, CUSTOM_ERROR_MESSAGE);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(IPV6));
+        assertEquals(CUSTOM_ERROR_MESSAGE, validator.getError(IPV6));
+    }
+
+    @Test
+    public void testIpv6Success() {
+        validator.add(IPV6, "2001:0db8:85a3:08d3:1319:8a2e:0370:7344");
+        validator.ipv6(IPV6);
+
+        assertFalse(validator.hasErrors());
+        assertFalse(validator.hasError(IPV6));
+    }
+
+    @Test
+    public void testRangeFailedString() {
+        validator.add(RANGE, "abcdef");
+        validator.range(RANGE, 8, 12);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(RANGE));
+        assertEquals("range must have a size between 8 and 12", validator.getError(RANGE));
+
+        validator.add(RANGE, "abcdef");
+        validator.range(RANGE, 8, 12, CUSTOM_ERROR_MESSAGE);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(RANGE));
+        assertEquals(CUSTOM_ERROR_MESSAGE, validator.getError(RANGE));
+    }
+
+    @Test
+    public void testRangeFailedNumeric() {
+        validator.add(RANGE, "4");
+        validator.range(RANGE, 8, 12);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(RANGE));
+        assertEquals("range must have a size between 8 and 12", validator.getError(RANGE));
+
+        validator.add(RANGE, "4");
+        validator.range(RANGE, 8, 12, CUSTOM_ERROR_MESSAGE);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(RANGE));
+        assertEquals(CUSTOM_ERROR_MESSAGE, validator.getError(RANGE));
+    }
+
+    @Test
+    public void testRangeSuccessString() {
+        validator.add(RANGE, "abcdefg");
+        validator.range(RANGE, 4, 10);
+
+        assertFalse(validator.hasErrors());
+        assertFalse(validator.hasError(RANGE));
+    }
+
+    @Test
+    public void testRangeSuccessNumeric() {
+        validator.add(RANGE, "6");
+        validator.range(RANGE, 4, 10);
+
+        assertFalse(validator.hasErrors());
+        assertFalse(validator.hasError(RANGE));
+    }
+
+    @Test
+    public void testRegexFailed() {
+        validator.add(REGEX, "abc03");
+        validator.regex(REGEX, Pattern.compile("[a-z]"));
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(REGEX));
+        assertEquals("regex is invalid", validator.getError(REGEX));
+
+        validator.add(REGEX, "abc03");
+        validator.regex(REGEX, Pattern.compile("[a-z]"), CUSTOM_ERROR_MESSAGE);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(REGEX));
+        assertEquals(CUSTOM_ERROR_MESSAGE, validator.getError(REGEX));
+    }
+
+    @Test
+    public void testRegexSuccess() {
+        validator.add(REGEX, "abc");
+        validator.regex(REGEX, Pattern.compile("^[A-Za-z0-9_.]+$"));
+
+        assertFalse(validator.hasErrors());
+        assertFalse(validator.hasError(REGEX));
+    }
+
+    @Test
+    public void testUrlFailed() {
+        validator.add(URL, "http:/mangoo.io");
+        validator.url(URL);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(URL));
+        assertEquals("url must be a valid URL", validator.getError(URL));
+
+        validator.add(URL, "http:/mangoo.io");
+        validator.url(URL, CUSTOM_ERROR_MESSAGE);
+
+        assertTrue(validator.hasErrors());
+        assertTrue(validator.hasError(URL));
+        assertEquals(CUSTOM_ERROR_MESSAGE, validator.getError(URL));
+    }
+
+    @Test
+    public void testUrlSuccess() {
+        validator.add(URL, "https://mangoo.io");
+        validator.url(URL);
+
+        assertFalse(validator.hasErrors());
+        assertFalse(validator.hasError(URL));
     }
 }
