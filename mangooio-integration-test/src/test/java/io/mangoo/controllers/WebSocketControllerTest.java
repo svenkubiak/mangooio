9095449249690324564a710304f49a65f7535aa4
Sven Kubiak <sk@svenkubiak.de>
2015-09-17 11:52:51 +0200

    updated websocket unit tests

--- mangooio-integration-test/src/test/java/io/mangoo/controllers/WebSocketControllerTest.java
+++ mangooio-integration-test/src/test/java/io/mangoo/controllers/WebSocketControllerTest.java
@@ -19,45 +19,50 @@ import io.mangoo.enums.Default;
 import io.mangoo.enums.Key;
 
 /**
- * 
+ *
  * @author svenkubiak
  *
  */
 public class WebSocketControllerTest {
     private static String host;
     private static int port;
-    
+
     @Before
     public void init() {
         Application.getInjector();
         Config config = Application.getInjector().getInstance(Config.class);
-        
+
         host = config.getString(Key.APPLICATION_HOST, Default.APPLICATION_HOST.toString());
         port = config.getInt(Key.APPLICATION_PORT, Default.APPLICATION_PORT.toInt());
     }
-  
+
     @Test
     public void testStartAndBuild() throws Exception {
         WebSocketClientFactory factory = new WebSocketClientFactory();
         factory.start();
-        
+
         WebSocketClient client = new WebSocketClient(factory);
-        
+
         WebSocket.Connection connection = client.open(new URI("ws://" + host + ":" + port + "/websocket"), new WebSocket.OnTextMessage() {
+            @Override
             public void onOpen(Connection connection) {
-               // open notification
-             }
-        
-             public void onClose(int closeCode, String message) {
-               // close notification
-             }
-        
-             public void onMessage(String data) {
-               // handle incoming message
-             }
-         }).get(5, TimeUnit.SECONDS);
-        
-        connection.sendMessage("Hello World");
+                // open notification
+            }
+
+            @Override
+            public void onClose(int closeCode, String message) {
+                // close notification
+            }
+
+            @Override
+            public void onMessage(String data) {
+                // handle incoming message
+            }
+        }).get(5, TimeUnit.SECONDS);
+
+        String message = "Hello World";
+        connection.sendMessage(message);
+        connection.sendMessage(message.getBytes(), 0, 0);
         assertThat(connection, is(notNullValue()));
         connection.close();
     }
