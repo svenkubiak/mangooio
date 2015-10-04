c998364292c77add97e4a557a044b9444960d783
Sven Kubiak <sk@svenkubiak.de>
2015-10-04 21:12:39 +0200

    minor refactoring

--- mangooio-core/src/main/java/io/mangoo/routing/handlers/ExceptionHandler.java
+++ mangooio-core/src/main/java/io/mangoo/routing/handlers/ExceptionHandler.java
@@ -19,25 +19,35 @@ import io.undertow.util.StatusCodes;
 public class ExceptionHandler implements HttpHandler {
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, ContentType.TEXT_HTML.toString());
-        exchange.getResponseHeaders().put(Header.X_XSS_PPROTECTION.toHttpString(), Default.XSS_PROTECTION.toInt());
-        exchange.getResponseHeaders().put(Header.X_CONTENT_TYPE_OPTIONS.toHttpString(), Default.NOSNIFF.toString());
-        exchange.getResponseHeaders().put(Header.X_FRAME_OPTIONS.toHttpString(), Default.SAMEORIGIN.toString());
-        exchange.getResponseHeaders().put(Headers.SERVER, Default.SERVER.toString());
-        exchange.setResponseCode(StatusCodes.INTERNAL_SERVER_ERROR);
+    	Throwable throwable = null; 
+    	try {
+    		throwable = exchange.getAttachment(io.undertow.server.handlers.ExceptionHandler.THROWABLE);
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, ContentType.TEXT_HTML.toString());
+            exchange.getResponseHeaders().put(Header.X_XSS_PPROTECTION.toHttpString(), Default.XSS_PROTECTION.toInt());
+            exchange.getResponseHeaders().put(Header.X_CONTENT_TYPE_OPTIONS.toHttpString(), Default.NOSNIFF.toString());
+            exchange.getResponseHeaders().put(Header.X_FRAME_OPTIONS.toHttpString(), Default.SAMEORIGIN.toString());
+            exchange.getResponseHeaders().put(Headers.SERVER, Default.SERVER.toString());
+            exchange.setResponseCode(StatusCodes.INTERNAL_SERVER_ERROR);
 
-        if (Application.inDevMode()) {
-            TemplateEngine templateEngine = Application.getInjector().getInstance(TemplateEngine.class);
-            Throwable throwable = exchange.getAttachment(io.undertow.server.handlers.ExceptionHandler.THROWABLE);
-            if (throwable == null) {
-                exchange.getResponseSender().send(Template.DEFAULT.internalServerError());
-            } else if (throwable.getCause() == null) {
-                exchange.getResponseSender().send(templateEngine.renderException(exchange, throwable, true));
+            if (Application.inDevMode()) {
+                TemplateEngine templateEngine = Application.getInjector().getInstance(TemplateEngine.class);
+                
+                if (throwable == null) {
+                    exchange.getResponseSender().send(Template.DEFAULT.internalServerError());
+                } else if (throwable.getCause() == null) {
+                    exchange.getResponseSender().send(templateEngine.renderException(exchange, throwable, true));
+                } else {
+                    exchange.getResponseSender().send(templateEngine.renderException(exchange, throwable.getCause(), false));
+                }
             } else {
-                exchange.getResponseSender().send(templateEngine.renderException(exchange, throwable.getCause(), false));
+                exchange.getResponseSender().send(Template.DEFAULT.internalServerError());
             }
-        } else {
-            exchange.getResponseSender().send(Template.DEFAULT.internalServerError());
-        }
+    	} catch (Exception e) {
+    		if (throwable ==  null) {
+    			e.printStackTrace();
+    		} else {
+    			throwable.printStackTrace();
+    		}
+    	}
     }
 }
