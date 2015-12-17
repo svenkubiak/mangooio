c935485bc12a7c188e4cac70a072acade2d4bc2f
Sven Kubiak <sk@svenkubiak.de>
2015-12-17 08:03:06 +0100

    sonar refactorings

--- mangooio-core/src/main/java/io/mangoo/utils/JsonUtils.java
+++ mangooio-core/src/main/java/io/mangoo/utils/JsonUtils.java
@@ -12,9 +12,12 @@ import org.boon.json.ObjectMapper;
  * @author svenkubiak
  *
  */
-public class JsonUtils {
-    private static JsonSerializer jsonSerializer = createJsonSerializer();
+public final class JsonUtils {
+    private static volatile JsonSerializer jsonSerializer = createJsonSerializer();
     private static ObjectMapper objectMapper = JsonFactory.create();
+    
+    private JsonUtils(){
+    }
     
     private static JsonSerializer createJsonSerializer() {
         JsonSerializerFactory jsonSerializerFactory = new JsonSerializerFactory();
@@ -22,22 +25,46 @@ public class JsonUtils {
         return jsonSerializerFactory.create();
     }
     
+    /**
+     * Sets a new custom JsonSerializer
+     * 
+     * @param serializer The JsonSerializer to set
+     */
     public static void withJsonSerializer(JsonSerializer serializer) {
         jsonSerializer = serializer;
     }
     
+    /**
+     * Converts a given object to a Json string
+     * 
+     * @param object The object to convert
+     * @return json string
+     */
     public static String toJson(Object object) {
         Objects.requireNonNull(object, "object can not be null");
         
         return jsonSerializer.serialize(object).toString();
     }
     
+    /**
+     * Converts a given Json string to an object
+     * 
+     * @param json The json string to convert
+     * @return The converted object
+     */
     public static Object fromJson(String json) {
         Objects.requireNonNull(json, "json can not be null");
         
         return objectMapper.fromJson(json);
     }
     
+    /**
+     * Converts a given Json string to given Class
+     * 
+     * @param json The json string to convert
+     * @param clazz The Class to converz to
+     * @return The converted class
+     */
     public static <T> T fromJson(String json, Class<T> clazz) {
         Objects.requireNonNull(json, "json can not be null");
         Objects.requireNonNull(clazz, "clazz can not be null");
