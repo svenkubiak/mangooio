a56377969def13b8c807a488a8a5628680709b27
Sven Kubiak <sk@svenkubiak.de>
2016-01-13 11:30:29 +0100

    refactored LocaleHandler

--- mangooio-core/src/main/java/io/mangoo/routing/handlers/LocaleHandler.java
+++ mangooio-core/src/main/java/io/mangoo/routing/handlers/LocaleHandler.java
@@ -1,9 +1,8 @@
 package io.mangoo.routing.handlers;
 
 import java.util.Locale;
-import java.util.Optional;
 
-import com.google.common.base.Splitter;
+import org.apache.commons.lang3.StringUtils;
 
 import io.mangoo.routing.RequestAttachment;
 import io.mangoo.utils.RequestUtils;
@@ -11,6 +10,7 @@ import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HeaderValues;
 import io.undertow.util.Headers;
+import io.undertow.util.LocaleUtils;
 
 /**
  *
@@ -23,19 +23,12 @@ public class LocaleHandler implements HttpHandler {
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         final RequestAttachment requestAttachment = exchange.getAttachment(RequestUtils.REQUEST_ATTACHMENT);
         final HeaderValues headerValues = exchange.getRequestHeaders().get(Headers.ACCEPT_LANGUAGE_STRING);
-
-        Locale locale = null;
-        String defaultLanguage = requestAttachment.getConfig().getApplicationLanguage();
-        if (headerValues == null) {
-            locale = Locale.forLanguageTag(defaultLanguage);
-        } else if (headerValues.getFirst() != null) {
-            final String values = Optional.ofNullable(headerValues.getFirst()).orElse("");
-            final Iterable<String> splitter = Splitter.on(",").trimResults().split(values);
-            if (splitter == null) {
-                locale = Locale.forLanguageTag(defaultLanguage);
-            } else {
-                final String acceptLanguage = Optional.ofNullable(splitter.iterator().next()).orElse(defaultLanguage);
-                locale = Locale.forLanguageTag(acceptLanguage.substring(0, 2)); //NOSONAR
+        Locale locale = Locale.forLanguageTag(requestAttachment.getConfig().getApplicationLanguage());
+        
+        if (headerValues != null) {
+            String acceptLanguage = headerValues.element();
+            if (StringUtils.isNotBlank(acceptLanguage)) {
+                locale = LocaleUtils.getLocaleFromString(acceptLanguage);
             }
         }
 
