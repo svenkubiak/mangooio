60e64f0f659e9251f29087716801fe03f80bd7c7
Sven Kubiak <sk@svenkubiak.de>
2016-05-03 09:24:44 +0200

    #104 Fixed failing unit tests

--- mangooio-integration-test/src/test/java/io/mangoo/controllers/AdminControllerTest.java
+++ mangooio-integration-test/src/test/java/io/mangoo/controllers/AdminControllerTest.java
@@ -18,20 +18,18 @@ import io.undertow.util.StatusCodes;
  *
  */
 public class AdminControllerTest {
-//    private static final String TEXT_HTML = "text/html; charset=UTF-8";
+    private static final String TEXT_HTML = "text/html; charset=UTF-8";
     private static final String TEXT_PLAIN = "text/plain; charset=UTF-8";
-//    private static final String SCHEDULER = "scheduler";
-//    private static final String METRICS = "metrics";
-//    private static final String CACHE = "cache";
-//    private static final String ROUTES = "routes";
-//    private static final String CONFIG = "config";
-    private static final String ALIVE = "alive";
+    private static final String SCHEDULER = "scheduler";
+    private static final String METRICS = "metrics";
+    private static final String CACHE = "cache";
+    private static final String ROUTES = "routes";
+    private static final String CONFIG = "config";
     private static final String ADMIN = "admin";
-//    private static final String PROPERTIES = "properties";
-//    private static final String MEMORY = "memory";    
-
+    private static final String PROPERTIES = "properties";
+    
     @Test
-    public void testAdminAuthorized() {
+    public void testDashboardAuthorized() {
         //given
         HTTPResponse response = HTTPRequest.get("/@admin")
                 .withBasicauthentication(ADMIN, ADMIN)
@@ -40,12 +38,12 @@ public class AdminControllerTest {
         //then
         assertThat(response, not(nullValue()));
         assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
-        assertThat(response.getContentType(), equalTo(TEXT_PLAIN));
-        assertThat(response.getContent(), containsString(ALIVE));
+        assertThat(response.getContentType(), equalTo(TEXT_HTML));
+        assertThat(response.getContent(), containsString(PROPERTIES));
     }
     
     @Test
-    public void testAdminUnaothorized() {
+    public void testDashboardUnAuthorized() {
         //given
         HTTPResponse response = HTTPRequest.get("/@admin").execute();
         
@@ -53,6 +51,136 @@ public class AdminControllerTest {
         assertThat(response, not(nullValue()));
         assertThat(response.getStatusCode(), equalTo(StatusCodes.UNAUTHORIZED));
         assertThat(response.getContentType(), equalTo(TEXT_PLAIN));
-        assertThat(response.getContent(), not(containsString(ALIVE)));
+        assertThat(response.getContent(), not(containsString(PROPERTIES)));
+    }
+    
+    @Test
+    public void testConfigAuthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/configuration")
+                .withBasicauthentication(ADMIN, ADMIN)
+                .execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
+        assertThat(response.getContentType(), equalTo(TEXT_HTML));
+        assertThat(response.getContent(), containsString(CONFIG));
+    }
+    
+    @Test
+    public void testConfigUnauthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/configuration").execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.UNAUTHORIZED));
+        assertThat(response.getContentType(), equalTo(TEXT_PLAIN));
+        assertThat(response.getContent(), not(containsString(CONFIG)));
+    }
+
+    @Test
+    public void testRoutedAuthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/routes")
+                .withBasicauthentication(ADMIN, ADMIN)
+                .execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
+        assertThat(response.getContentType(), equalTo(TEXT_HTML));
+        assertThat(response.getContent(), containsString(ROUTES));
+    }
+    
+    @Test
+    public void testRoutedUnauthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/routes").execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.UNAUTHORIZED));
+        assertThat(response.getContentType(), equalTo(TEXT_PLAIN));
+        assertThat(response.getContent(), not(containsString(ROUTES)));
+    }
+
+    @Test
+    public void testCacheAuthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/cache")
+                .withBasicauthentication(ADMIN, ADMIN)
+                .execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
+        assertThat(response.getContentType(), equalTo(TEXT_HTML));
+        assertThat(response.getContent(), containsString(CACHE));
+    }
+    
+    @Test
+    public void testCacheUnauthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/cache").execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.UNAUTHORIZED));
+        assertThat(response.getContentType(), equalTo(TEXT_PLAIN));
+        assertThat(response.getContent(), not(containsString(CACHE)));
+    }
+
+    @Test
+    public void testMetricsAuthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/metrics")
+                .withBasicauthentication(ADMIN, ADMIN)
+                .execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
+        assertThat(response.getContentType(), equalTo(TEXT_HTML));
+        assertThat(response.getContent(), containsString(METRICS));
+    }
+    
+    @Test
+    public void testMetricsUnauthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/metrics").execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.UNAUTHORIZED));
+        assertThat(response.getContentType(), equalTo(TEXT_PLAIN));
+        assertThat(response.getContent(), not(containsString(METRICS)));
+    }
+    
+    @Test
+    public void testSchedulerAuthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/scheduler")
+                .withBasicauthentication(ADMIN, ADMIN)
+                .execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
+        assertThat(response.getContentType(), equalTo(TEXT_HTML));
+        assertThat(response.getContent(), containsString(SCHEDULER));
+    }
+    
+    @Test
+    public void testSchedulerUnAuthorized() {
+        //given
+        HTTPResponse response = HTTPRequest.get("/@admin/scheduler").execute();
+        
+        //then
+        assertThat(response, not(nullValue()));
+        assertThat(response.getStatusCode(), equalTo(StatusCodes.UNAUTHORIZED));
+        assertThat(response.getContentType(), equalTo(TEXT_PLAIN));
+        assertThat(response.getContent(), not(containsString(SCHEDULER)));
     }
 }
