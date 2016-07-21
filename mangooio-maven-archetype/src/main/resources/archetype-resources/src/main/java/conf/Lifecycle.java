7c97a2557cba2c6d31b3fbd91cc6bed3bb5f6432
Sven Kubiak <sk@svenkubiak.de>
2016-07-21 16:55:44 +0200

    reverted lifecycle, need to find another way

--- mangooio-maven-archetype/src/main/resources/archetype-resources/src/main/java/conf/Lifecycle.java
+++ mangooio-maven-archetype/src/main/resources/archetype-resources/src/main/java/conf/Lifecycle.java
@@ -14,12 +14,4 @@ public class Lifecycle implements MangooLifecycle {
     @Override
     public void applicationInitialized() {
     }
-
-    @Override
-    public void applicationStopped() {
-    }
-
-    @Override
-    public void requestCompleted(String url, int status, int processTime, long bytesSend) {
-    }
 }
