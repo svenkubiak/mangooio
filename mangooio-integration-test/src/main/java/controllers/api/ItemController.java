be516f17aad29a04bdb50fd0827ebe98e453096e
Gowthaman Basuvaraj <gowthaman@365build.in>
2017-03-12 21:25:22 +0530

    cleanup tests

--- mangooio-integration-test/src/main/java/controllers/api/ItemController.java
+++ mangooio-integration-test/src/main/java/controllers/api/ItemController.java
@@ -1,32 +1,14 @@
 package controllers.api;
 
 import io.mangoo.routing.Response;
-import models.Item;
 
 import javax.inject.Singleton;
-import java.util.ArrayList;
-import java.util.List;
 
-/**
- * Created by gowthaman on 12/3/17.
- */
 @Singleton
 public class ItemController {
-    private static final List<Item> items = new ArrayList<>();
 
     public Response getItem() {
-        return Response.withOk().andJsonBody(items);
-    }
-
-    public Response createItem(Item item) {
         return Response.withOk().andEmptyBody();
     }
 
-    public Response updateItem() {
-        return Response.withOk().andEmptyBody();
-    }
-
-    public Response deleteItem(String name) {
-        return Response.withOk().andEmptyBody();
-    }
 }
--- mangooio-integration-test/src/test/java/io/mangoo/controllers/api/ItemControllerTest.java
+++ mangooio-integration-test/src/test/java/io/mangoo/controllers/api/ItemControllerTest.java
@@ -1,6 +1,5 @@
 package io.mangoo.controllers.api;
 
-import io.mangoo.enums.ContentType;
 import io.mangoo.test.utils.WebRequest;
 import io.mangoo.test.utils.WebResponse;
 import io.undertow.util.StatusCodes;
@@ -9,54 +8,12 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-/**
- * Created by gowthaman on 12/3/17.
- */
 public class ItemControllerTest {
-
 
     @Test
     public void testSubPackageGet() {
         //given
         WebResponse response = WebRequest.get("/api/item").execute();
-
-        //then
-        assertThat(response, not(nullValue()));
-        assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
-    }
-
-    @Test
-    public void testSubPackagePost() {
-        //given
-        WebResponse response = WebRequest.post("/api/item")
-                .withRequestBody("{\"name\":\"One\"}")
-                .withContentType(ContentType.APPLICATION_JSON)
-                .execute();
-
-        //then
-        assertThat(response, not(nullValue()));
-        assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
-        assertThat(response.getContent(), equalTo(""));
-    }
-
-
-    @Test
-    public void testSubPackagePut() {
-        //given
-        WebResponse response = WebRequest
-                .put("/api/item?name=One&status=false")
-                .execute();
-
-        //then
-        assertThat(response, not(nullValue()));
-        assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
-        assertThat(response.getContent(), equalTo(""));
-    }
-
-    @Test
-    public void testSubPackageDelete() {
-        //given
-        WebResponse response = WebRequest.delete("/api/item?name=One").execute();
 
         //then
         assertThat(response, not(nullValue()));
