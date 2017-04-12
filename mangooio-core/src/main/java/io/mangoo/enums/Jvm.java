897465e249bf509e1a4f05510c65790c4019e4da
Sven Kubiak <sk@svenkubiak.de>
2017-04-12 10:20:58 +0200

    Added deprecateions

--- mangooio-core/src/main/java/io/mangoo/enums/Jvm.java
+++ mangooio-core/src/main/java/io/mangoo/enums/Jvm.java
@@ -10,10 +10,15 @@ public enum Jvm {
     APPLICATION_CONFIG("application.config"),
     APPLICATION_MODE("application.mode"),
     APPLICATION_LOG("application.log"),
-    APPLICATION_MASTERKEY("application.masterkey"),    
+    APPLICATION_MASTERKEY("application.masterkey"),   
+    
+    @Deprecated /** Will be removed in 5.0.0 **/
     HTTP_HOST("http.host"),
+    @Deprecated /** Will be removed in 5.0.0 **/
     HTTP_PORT("http.port"),
+    @Deprecated /** Will be removed in 5.0.0 **/
     AJP_HOST("ajp.host"),
+    @Deprecated /** Will be removed in 5.0.0 **/
     AJP_PORT("ajp.port"),;
 
     private final String value;
