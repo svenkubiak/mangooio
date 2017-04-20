5bd47721406d54a85e852971ad9b40aa977a64cf
Sven Kubiak <sk@svenkubiak.de>
2017-04-20 11:42:11 +0200

    Added a new class for concurrency testing and unit test for concurrent json parsing

---
+++ mangooio-integration-test/src/test/java/io/mangoo/controllers/ConcurrentControllerTest.java
@@ -0,0 +1,51 @@
+package io.mangoo.controllers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+
+import java.util.UUID;
+
+import org.junit.Test;
+
+import io.mangoo.enums.ContentType;
+import io.mangoo.test.utils.ConcurrentRunner;
+import io.mangoo.test.utils.WebRequest;
+import io.mangoo.test.utils.WebResponse;
+import io.undertow.util.StatusCodes;
+
+/**
+ * 
+ * @author svenkubiak
+ *
+ */
+public class ConcurrentControllerTest {
+
+    @Test
+    public void testConcurrentJsonParsing() {
+        Runnable runnable = () -> {
+            for (int j=0; j < 50; j++) {
+                //given
+                String uuid = UUID.randomUUID().toString();
+                String json = "{\"firstname\":\"$$\",\"lastname\":\"Parker\",\"age\":24}";
+                json = json.replace("$$", uuid);
+                
+                WebResponse response = WebRequest.post("/parse")
+                        .withContentType(ContentType.APPLICATION_JSON)
+                        .withRequestBody(json)
+                        .execute();
+
+                //then
+                assertThat(response, not(nullValue()));
+                assertThat(response.getStatusCode(), equalTo(StatusCodes.OK));
+                assertThat(response.getContent(), equalTo(uuid + ";Parker;24"));    
+            }
+        };
+        
+        ConcurrentRunner.create()
+            .withRunnable(runnable)
+            .withThreads(50)
+            .run();
+    }
+}
