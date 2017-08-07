260181bf85f902cfb117ea6ff2e49d18d5c116e9
Sven Kubiak <sk@svenkubiak.de>
2017-08-07 14:16:39 +0200

    Sonar refactorings

--- mangooio-core/src/main/java/io/mangoo/utils/TotpUtils.java
+++ mangooio-core/src/main/java/io/mangoo/utils/TotpUtils.java
@@ -24,101 +24,101 @@ import io.mangoo.enums.Required;
  */
 public class TotpUtils {
     private static final Logger LOG = LogManager.getLogger(TotpUtils.class);
-	private static final Base32 base32 = new Base32();
+    private static final Base32 base32 = new Base32();
     private static final int DIGITS = 6;
     private static final int MAX_CHARACTERS = 32;
     private static final int THIRTY_SECONDS = 30;
-	private static final int ITERATIONS = 26;
+    private static final int ITERATIONS = 26;
     private static final int BYTES_SECRET = 64;
     
     private TotpUtils() {
     }
-	
+    
     /**
      * Generates a 64 byte (512 bit) secret, best used with HMAC_SHA512
      * 
      * @return A 64 characters random string based on SecureRandom
      */
-	public static Optional<String> createSecret() {
-		StringBuilder stringBuilder = new StringBuilder(BYTES_SECRET);
-		Random random = new SecureRandom();
-		for (int i = 0; i < BYTES_SECRET; i++) {
-			int value = random.nextInt(MAX_CHARACTERS);
-			if (value < ITERATIONS) {
-				stringBuilder.append((char) ('A' + value));
-			} else {
-				stringBuilder.append((char) ('2' + (value - ITERATIONS)));
-			}
-		}
-		
-		return Optional.of(stringBuilder.toString());
-	}
-	
-	/**
-	 * Creates the current TOTP based on the given secret and HMAC algorithm
-	 * 
-	 * @param secret The secret to use
-	 * @param hmacShaAlgorithm The HMAC algorithm to use
-	 * @return
-	 */
-	public static Optional<String> getTotp(String secret, HmacShaAlgorithm hmacShaAlgorithm) {
-		Objects.requireNonNull(secret, Required.SECRET.toString());
-		
-		String value = null;
-		try {
-			TOTP builder = TOTP.key(secret.getBytes(Charsets.US_ASCII.toString()))
-					.timeStep(TimeUnit.SECONDS.toMillis(THIRTY_SECONDS))
-					.digits(DIGITS)
-					.hmacSha(hmacShaAlgorithm)
-					.build();
-			
-			value = builder.value();
-		} catch (UnsupportedEncodingException e) {
-			LOG.error("Failed to create TOTP",  e);
-		}
-		
-		return Optional.ofNullable(value);
-	}
-	
-	/**
-	 * Verifies a given TOTP based on a given secret and HMAC algorithm
-	 * 
-	 * @param secret The secret to use
-	 * @param totp The TOTP to verify
- 	 * @param hmacShaAlgorithm The HMAC algorithm to use
- 	 * 
-	 * @return True if the TOTP is valid, false otherwise
-	 */
-	public static boolean verifiedTotp(String secret, String totp, HmacShaAlgorithm hmacShaAlgorithm) {
-		Objects.requireNonNull(secret, Required.SECRET.toString());
-		Objects.requireNonNull(totp, Required.TOTP.toString());
-		
-		String value = null;
-		try {
-			TOTP builder = TOTP.key(secret.getBytes(Charsets.US_ASCII.toString()))
-				.timeStep(TimeUnit.SECONDS.toMillis(THIRTY_SECONDS))
-				.digits(DIGITS)
-				.hmacSha(hmacShaAlgorithm)
-				.build();
-			
-			value = builder.value();
-		} catch (UnsupportedEncodingException e) {
-			LOG.error("Failed to verify TOTP",  e);
-		}
-		
-		return totp.equals(value);
-	}
-	
-	/**
-	 * Generates a QR code to share a secret with a user
-	 * 
-	 * @param name The name of the account
-	 * @param issuer The name of the issuer
-	 * @param secret The secret to use
-	 * @param hmacShaAlgorithm The HMAC algorithm to use
-	 * 
-	 * @return An URL to google charts API with the QR code
-	 */
+    public static Optional<String> createSecret() {
+        StringBuilder stringBuilder = new StringBuilder(BYTES_SECRET);
+        Random random = new SecureRandom();
+        for (int i = 0; i < BYTES_SECRET; i++) {
+            int value = random.nextInt(MAX_CHARACTERS);
+            if (value < ITERATIONS) {
+                stringBuilder.append((char) ('A' + value));
+            } else {
+                stringBuilder.append((char) ('2' + (value - ITERATIONS)));
+            }
+        }
+        
+        return Optional.of(stringBuilder.toString());
+    }
+    
+    /**
+     * Creates the current TOTP based on the given secret and HMAC algorithm
+     * 
+     * @param secret The secret to use
+     * @param hmacShaAlgorithm The HMAC algorithm to use
+     * @return
+     */
+    public static Optional<String> getTotp(String secret, HmacShaAlgorithm hmacShaAlgorithm) {
+        Objects.requireNonNull(secret, Required.SECRET.toString());
+        
+        String value = null;
+        try {
+            TOTP builder = TOTP.key(secret.getBytes(Charsets.US_ASCII.toString()))
+                    .timeStep(TimeUnit.SECONDS.toMillis(THIRTY_SECONDS))
+                    .digits(DIGITS)
+                    .hmacSha(hmacShaAlgorithm)
+                    .build();
+            
+            value = builder.value();
+        } catch (UnsupportedEncodingException e) {
+            LOG.error("Failed to create TOTP",  e);
+        }
+        
+        return Optional.ofNullable(value);
+    }
+    
+    /**
+     * Verifies a given TOTP based on a given secret and HMAC algorithm
+     * 
+     * @param secret The secret to use
+     * @param totp The TOTP to verify
+      * @param hmacShaAlgorithm The HMAC algorithm to use
+      * 
+     * @return True if the TOTP is valid, false otherwise
+     */
+    public static boolean verifiedTotp(String secret, String totp, HmacShaAlgorithm hmacShaAlgorithm) {
+        Objects.requireNonNull(secret, Required.SECRET.toString());
+        Objects.requireNonNull(totp, Required.TOTP.toString());
+        
+        String value = null;
+        try {
+            TOTP builder = TOTP.key(secret.getBytes(Charsets.US_ASCII.toString()))
+                .timeStep(TimeUnit.SECONDS.toMillis(THIRTY_SECONDS))
+                .digits(DIGITS)
+                .hmacSha(hmacShaAlgorithm)
+                .build();
+            
+            value = builder.value();
+        } catch (UnsupportedEncodingException e) {
+            LOG.error("Failed to verify TOTP",  e);
+        }
+        
+        return totp.equals(value);
+    }
+    
+    /**
+     * Generates a QR code to share a secret with a user
+     * 
+     * @param name The name of the account
+     * @param issuer The name of the issuer
+     * @param secret The secret to use
+     * @param hmacShaAlgorithm The HMAC algorithm to use
+     * 
+     * @return An URL to google charts API with the QR code
+     */
     public static String getQRCode(String name, String issuer, String secret, HmacShaAlgorithm hmacShaAlgorithm) {
         Objects.requireNonNull(name, Required.ACCOUNT_NAME.toString());
         Objects.requireNonNull(secret, Required.SECRET.toString());
@@ -136,11 +136,11 @@ public class TotpUtils {
     /**
      * Generates a otpauth code to share a secret with a user
      * 
-	 * @param name The name of the account
-	 * @param issuer The name of the issuer
-	 * @param secret The secret to use
-	 * @param hmacShaAlgorithm The HMAC algorithm to use
-	 * 
+     * @param name The name of the account
+     * @param issuer The name of the issuer
+     * @param secret The secret to use
+     * @param hmacShaAlgorithm The HMAC algorithm to use
+     * 
      * @return An otpauth url
      */
     public static String getOtpauthURL(String name, String issuer, String secret, HmacShaAlgorithm hmacShaAlgorithm) {
@@ -149,10 +149,10 @@ public class TotpUtils {
             .append(name)
             .append("?secret=")
             .append(base32.encodeAsString(secret.getBytes(Charsets.UTF_8)).replaceAll("=", ""))
-        		.append("&algorithm=")
-        		.append(hmacShaAlgorithm.getAlgorithm())
-        		.append("&issuer=")
-        		.append(issuer);
+                .append("&algorithm=")
+                .append(hmacShaAlgorithm.getAlgorithm())
+                .append("&issuer=")
+                .append(issuer);
         
         return buffer.toString();
     }
