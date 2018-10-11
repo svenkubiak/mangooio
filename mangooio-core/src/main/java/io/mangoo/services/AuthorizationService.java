e5a069148a491639bb51dd75f515fa498c5df1df
Sven Kubiak <sk@svenkubiak.de>
2018-10-11 11:46:07 +0200

    Disabled jCasbin logging

--- mangooio-core/src/main/java/io/mangoo/services/AuthorizationService.java
+++ mangooio-core/src/main/java/io/mangoo/services/AuthorizationService.java
@@ -1,6 +1,7 @@
 package io.mangoo.services;
 
 import org.casbin.jcasbin.main.Enforcer;
+import org.casbin.jcasbin.util.Util;
 
 import com.google.common.io.Resources;
 import com.google.inject.Singleton;
@@ -18,10 +19,12 @@ public class AuthorizationService {
     private Enforcer enforcer;
 
     public AuthorizationService () {
+        Util.enableLog = false;
         if (MangooUtils.resourceExists(Default.MODEL_CONF.toString()) && MangooUtils.resourceExists(Default.POLICY_CSV.toString())) {
-            this.enforcer = new Enforcer(Resources.getResource(Default.MODEL_CONF.toString()).getPath(), Resources.getResource(Default.POLICY_CSV.toString()).getPath());            
+            this.enforcer = new Enforcer(Resources.getResource(Default.MODEL_CONF.toString()).getPath(), Resources.getResource(Default.POLICY_CSV.toString()).getPath(), false);            
         } else {
             this.enforcer = new Enforcer();
+            this.enforcer.enableLog(false);
         }
     }
     
