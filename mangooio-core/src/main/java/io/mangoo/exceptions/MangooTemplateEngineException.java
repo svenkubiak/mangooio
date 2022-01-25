092d444d406272d46dd72ae3cd21c8cc94c9bc4e
Sven Kubiak <sk@svenkubiak.de>
2022-01-25 11:55:44 +0100

    Sonar refactorings

--- mangooio-core/src/main/java/io/mangoo/exceptions/MangooTemplateEngineException.java
+++ mangooio-core/src/main/java/io/mangoo/exceptions/MangooTemplateEngineException.java
@@ -1,16 +1,13 @@
 package io.mangoo.exceptions;
 
-import java.io.Serial;
-
 /**
  * 
- * @author sven.kubiak
+ * @author svenkubiak
  *
  */
 public class MangooTemplateEngineException extends Exception {
-    @Serial
-    private static final long serialVersionUID = -3362035234167593528L;
-    
+    private static final long serialVersionUID = -5025696177592253242L;
+
     public MangooTemplateEngineException(String message, Exception e) {
         super(message, e);
     }
