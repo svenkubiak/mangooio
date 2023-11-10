7ea2abdbc18a7b6f0760bd3e19abb9acf7bede06
Sven Kubiak <sk@svenkubiak.de>
2023-11-10 08:52:45 +0100

    Build fix

--- mangooio-integration-test/src/test/java/io/mangoo/models/TestModel.java
+++ mangooio-integration-test/src/test/java/io/mangoo/models/TestModel.java
@@ -2,13 +2,11 @@ package io.mangoo.models;
 
 import java.io.Serializable;
 
-import io.mangoo.persistence.BaseModel;
 import io.mangoo.persistence.annotations.Collection;
 
 @Collection(name = "test")
-public class TestModel extends BaseModel implements Serializable {
-    private static final long serialVersionUID = -3974611906988154231L;
-    
+public class TestModel {
+
     private String name;
     
     public TestModel(){
