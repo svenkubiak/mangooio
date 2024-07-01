1b41c49c9f7e0aa0e82e775369f5088b1ab40dff
Sven Kubiak <sk@svenkubiak.de>
2024-07-01 10:16:14 +0200

    Added release script

---
+++ release.sh
@@ -0,0 +1,21 @@
+mvn clean verify
+mvn versions:set
+STATUS=$?
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+if [ $STATUS -ne 0 ]; then
+  echo "Failed to set new version!"
+else
+  mvn clean deploy -Prelease
+  STATUS=$?
+  if [ $STATUS -ne 0 ]; then
+    echo "Failed to release!"
+  else
+    git tag $VERSION
+    mvn release:update-versions
+    git commit -am "Updated version after release"
+    git push origin main
+  fi
+fi
+
+rm pom.xml.versionsBackup
