91a9b1c28fac243d265dde0c36cb79734eca3f24
Sven Kubiak <sk@svenkubiak.de>
2024-11-10 10:28:49 +0100

    Added additional unit test

--- mangooio-integration-test/src/test/java/io/mangoo/utils/ByteUtilsTest.java
+++ mangooio-integration-test/src/test/java/io/mangoo/utils/ByteUtilsTest.java
@@ -1,11 +1,14 @@
 package io.mangoo.utils;
 
+import io.mangoo.constants.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Execution(ExecutionMode.CONCURRENT)
 class ByteUtilsTest {
@@ -19,7 +22,21 @@ class ByteUtilsTest {
         assertThat(ByteUtils.bitLength(string.getBytes()), equalTo(184));
     }
 
-    
+    @Test
+    void testBitLengthAsStringNotNull() {
+        //given
+        String string = null;
+
+        //then
+        Exception exception = assertThrows(NullPointerException.class, () -> {
+            ByteUtils.bitLength(string);
+        });
+
+        //then
+        String actualMessage = exception.getMessage();
+        assertThat(actualMessage, containsString(NotNull.STRING));
+    }
+
     @Test
     void testBitLengthAsString() {
         //given
