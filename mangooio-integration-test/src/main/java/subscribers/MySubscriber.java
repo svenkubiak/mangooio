a5aa3e76f988ee77e1be04950a29973df0d673cc
Sven Kubiak <sk@svenkubiak.de>
2025-10-11 09:14:06 +0200

    Fixed failing unit test

--- mangooio-integration-test/src/main/java/subscribers/MySubscriber.java
+++ mangooio-integration-test/src/main/java/subscribers/MySubscriber.java
@@ -1,8 +1,10 @@
 package subscribers;
 
+import io.mangoo.annotations.Async;
 import io.mangoo.async.Subscriber;
 import utils.Utils;
 
+@Async
 public class MySubscriber implements Subscriber<String> {
     @Override
     public void receive(String payload) {
