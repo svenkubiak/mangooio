b2f86d65af0cfdc2632513bde669e84b3375e802
Sven Kubiak <sk@svenkubiak.de>
2026-04-26 08:53:51 +0200

    Improved Shutdown hook

--- mangooio-core/src/main/java/io/mangoo/core/Shutdown.java
+++ mangooio-core/src/main/java/io/mangoo/core/Shutdown.java
@@ -4,49 +4,79 @@ import io.mangoo.interfaces.MangooBootstrap;
 import io.mangoo.utils.internal.Trace;
 import jakarta.inject.Singleton;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Singleton
 public class Shutdown extends Thread {
+    private static final Logger LOG = LogManager.getLogger(Shutdown.class);
+
     public Shutdown() {
         // Empty constructor for Google Guice
     }
 
-    private static void invokeLifecycle() {
-        Application.getInstance(MangooBootstrap.class).applicationStopped();
-    }
-
-    private static void stopScheduler() {
-        if (Application.getInstance(Config.class).isSchedulerEnabled()) {
-            Application.getScheduledExecutorService().shutdown();
-            Application.getExecutorService().shutdown();
+    private void invokeLifecycle() {
+        try {
+            Application.getInstance(MangooBootstrap.class).applicationStopped();
+        } catch (Exception e) {
+            LOG.error("Error invoking lifecycle shutdown", e);
         }
     }
 
-    private static void stopUndertow() {
-        Application.stopUndertow();
-    }
-
-    private static void stopEmbeddedMongoDB() {
-        Application.stopEmbeddedMongoDB();
-    }
-
-    private static void stopTelemetry() {
-        if (Application.getInstance(Config.class).isOtlpEnable()) {
-            Trace.shutdown();
+    private void stopScheduler() {
+        try {
+            if (Application.getInstance(Config.class).isSchedulerEnabled()) {
+                LOG.info("Stopping Scheduler");
+                Application.getScheduledExecutorService().shutdown();
+                Application.getExecutorService().shutdown();
+            }
+        } catch (Exception e) {
+            LOG.error("Error stopping Scheduler", e);
         }
     }
 
-    private static void stopLogger() {
+    private void stopUndertow() {
+        try {
+            LOG.info("Stopping Undertow");
+            Application.stopUndertow();
+        } catch (Exception e) {
+            LOG.error("Error stopping Undertow", e);
+        }
+    }
+
+    private void stopEmbeddedMongoDB() {
+        try {
+            if (Application.getInstance(Config.class).isPersistenceEnabled()) {
+                LOG.info("Stopping EmbeddedMongoDB");
+                Application.stopEmbeddedMongoDB();
+            }
+        } catch (Exception e) {
+            LOG.error("Error stopping EmbeddedMongoDB", e);
+        }
+    }
+
+    private void stopTelemetry() {
+        try {
+            if (Application.getInstance(Config.class).isOtlpEnable()) {
+                LOG.info("Stopping Telemetry");
+                Trace.shutdown();
+            }
+        } catch (Exception e) {
+            LOG.error("Error stopping Telemetry", e);
+        }
+    }
+
+    private void stopLogger() {
+        LOG.info("Stopping Log4j");
         LogManager.shutdown();
     }
-    
+
     @Override
     public void run() {
-        Thread.ofVirtual().start(Shutdown::invokeLifecycle);
-        Thread.ofVirtual().start(Shutdown::stopUndertow);
-        Thread.ofVirtual().start(Shutdown::stopScheduler);
-        Thread.ofVirtual().start(Shutdown::stopEmbeddedMongoDB);
-        Thread.ofVirtual().start(Shutdown::stopTelemetry);
-        Thread.ofVirtual().start(Shutdown::stopLogger);
+        invokeLifecycle();
+        stopScheduler();
+        stopEmbeddedMongoDB();
+        stopTelemetry();
+        stopUndertow();
+        stopLogger();
     }
 }
